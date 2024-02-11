ad2e0cf6a7e1e6b5f465f8989b446fec440b3055
User User <stepkadmitrievichreg67@mail.ru>
2024-02-11 17:16:31 +0300

    добавлена кнопка 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,22 +29,33 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(83, 125);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(334, 221);
+            this.button1.Size = new System.Drawing.Size(185, 221);
             this.button1.TabIndex = 0;
             this.button1.Text = "король батон первый";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 221);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "кнопка 2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(845, 479);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,5 +66,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
